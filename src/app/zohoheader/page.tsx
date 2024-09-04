f8774d9fdb2c44ab69c3ab3c8a136912aa7c5b76
ramya72002 <nsriramya7@gmail.com>
2024-09-04 22:16:56 +0530

    layout page upfated

--- src/app/zohoheader/page.tsx
+++ src/app/zohoheader/page.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import './zohoheader.scss'; // Import the SCSS file
 
 const ZohoHeader = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <div className="header">
-      <a href="/zoho/dashboard" className="menu-item">Dashboard</a>
-      <a href="#" className="menu-item">Job Openings</a>
-      <a href="#" className="menu-item">Candidates</a>
-      <a href="#" className="menu-item">Interviews</a>
-      <a href="#" className="menu-item">Clients</a>
+      <div className="menu">
+        <a href="/zoho" className="menu-item">Dashboard</a>
+        <a href="/zoho/dashboard" className="menu-item">Job Openings</a>
+        <a href="/zoho/dashboard" className="menu-item">Candidates</a>
+        <a href="/zoho/dashboard" className="menu-item">Interviews</a>
+        <a href="/zoho/dashboard" className="menu-item">Clients</a>
+      </div>
+      <div className="plus-menu" onClick={toggleDropdown}>
+        +
+        {dropdownVisible && (
+          <div className="dropdown-content">
+            <a href="/zoho/dashboard">Job Openings</a>
+            <a href="/zoho/dashboard">Candidates</a>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
